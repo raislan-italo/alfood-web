9831ee067e9793faa786eec55c9fd1df1362ba3e
Raislan Ítalo <raislanitalo62@gmail.com>
2025-06-25 11:01:50 -0300

    adicionado melhorias no layout e nas requisições

---
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -0,0 +1,64 @@
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import IRestaurante from "../../../interfaces/IRestaurante"
+import { Link } from "react-router-dom"
+
+const AdministracaoRestaurantes = () => {
+
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+            .then(resposta => setRestaurantes(resposta.data))
+    }, [])
+
+    const excluir = (restauranteExcluido : IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteExcluido.id}/`)
+        .then(() => {
+            const listaRestaurantes = restaurantes.filter(restaurante => restaurante.id !== restauranteExcluido.id)
+            setRestaurantes([...listaRestaurantes])
+        })
+    }
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                        <TableCell>
+                            Eitar
+                        </TableCell>
+
+                        <TableCell>
+                            Excluir
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
+                        <TableCell>
+                            {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            [ <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link> ]
+                        </TableCell>
+
+                        <TableCell>
+                            <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                Excluir
+                            </Button>
+                        </TableCell>
+
+                    </TableRow>)}
+
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default AdministracaoRestaurantes
